e77b644154dcbcc737ac242837ce37877fe5763f
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-18 21:55:06 +0530

    solve a problem for dsa in java find the median for sorting array

---
+++ AllDSA/MedianArray.java
@@ -0,0 +1,24 @@
+package AllDSA;
+
+public class MedianArray {
+
+    static void medianfun(int arr1[], int arr2[]){
+int arr3[];
+int a = arr1.length;
+int b = arr2.length;
+int c = arr3.length;
+
+if(a>b){
+    arr3 = new int[a];
+}else{
+    
+}
+
+    }
+    public static void main(String[] args) {
+        int arr1[] = {1,9,5,8,3};
+        int arr2[] = {8,6,3,7,2};
+        median (arr1,arr2);
+    }
+    
+}
