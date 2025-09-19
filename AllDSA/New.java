292423a7f15925596ef7c492e7bdbdd360a006d5
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-19 23:24:45 +0530

    #

--- AllDSA/New.java
+++ AllDSA/New.java
@@ -29,33 +29,3 @@ public class New {
 }
 
 
-// package AllDSA;
-
-// public class Palindrome {
-
-//     static String palindromfind(String str) {
-//         int maxLength = 0;
-//         String longestPalindrome = "";
-
-//         for (int i = 0; i < str.length(); i++) {
-//             for (int j = i; j < str.length(); j++) {
-//                 String substring = str.substring(i, j + 1);
-
-//                 String reversed = new StringBuilder(substring).reverse().toString();
-
-//                 if (substring.equals(reversed) && substring.length() > maxLength) {
-//                     longestPalindrome = substring;
-//                     maxLength = substring.length();
-//                 }
-//             }
-//         }
-
-//         return longestPalindrome;
-//     }
-
-//     public static void main(String[] args) {
-//         String str = "greeksskeerg";
-//         String result = palindromfind(str);
-//         System.out.println("Longest Palindrome Substring is: " + result);
-//     }
-// }
