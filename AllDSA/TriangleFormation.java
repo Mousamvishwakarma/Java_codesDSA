657972a6d11c1314350f13a80e301b1f9e9a37ad
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-22 23:56:10 +0530

    Implemented Triangle Formation problem in Java to check if non-degenerate triangle is possible

--- AllDSA/TriangleFormation.java
+++ AllDSA/TriangleFormation.java
@@ -1,5 +1,29 @@
 package AllDSA;
+import java.util.Arrays;
 
 public class TriangleFormation {
-    
+    public static boolean canFormTriangle(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] + arr[i+1] > arr[i+2]) {
+                return true; // Triangle can be formed
+            }
+        }
+        return false; // No triangle possible
+    }
+
+    public static void main(String[] args) {
+        int[][] testCases = {
+            {3, 4, 5},
+            {1, 10, 12, 30}
+        };
+
+        for (int[] testCase : testCases) {
+            if (canFormTriangle(testCase)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
